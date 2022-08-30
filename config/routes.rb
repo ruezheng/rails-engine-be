3d89a486294a250cc03e7473bb9d8cab05b7c4f0
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-30 16:07:41 -0600

    Feat: add get request to merchant_items index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only[:index], to: 'merchant_items#index'
+      end
+      
       resources :items, only: [:index, :show, :create, :update, :destroy]
     end
   end
