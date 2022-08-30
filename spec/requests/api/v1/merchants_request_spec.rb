a79d6ae9882c53f4f555b6c8a9ad5a9cd5722846
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-30 09:59:03 -0600

    Add data key to merchants JSON response

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,39 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants API" do
-  describe 'index action' do
+  describe 'GET /api/v1/merchants endpoint' do
     context 'happy path' do
       it 'gets all merchants and their attributes' do
         create_list(:merchant, 3)
-        
+
         get '/api/v1/merchants'
-        
-        expect(response.status).to eq(200)      
-        
+
+        expect(response.status).to eq(200)
+
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
         expect(merchants.count).to eq(3)
         
         merchants.each do |merchant|
-          expect(merchant[:attributes]).to include(:id, :name) 
-          expect(merchant[:attributes][:id]).to be_an(Integer) 
-          expect(merchant[:attributes][:name]).to be_a(String) 
+          expect(merchant).to have_key(:id)
+          expect(merchant[:attributes][:id]).to be_an(Integer)
+          expect(merchant[:attributes]).to have_key(:name)
+          expect(merchant[:attributes][:name]).to be_a(String)
         end
       end
     end
-    
-    context 'sad path' do
-      expect(response).to_not have_key(:number_sold)
-      expect(error).to eq(null)
-    end
+
+    # context 'sad path' do
+    #   expect(response).to_not have_key(:number_sold)
+    #   expect(error).to eq(null)
+    # end
 
   end
 
   # describe 'show action' do
   #   it 'sends data for one merchant' do
-      
+
   #   end
   # end
 end
-
-  
