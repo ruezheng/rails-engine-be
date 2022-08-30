3141cd2db1c39dd49ee17096bf2b6e602e1131dd
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-30 11:10:43 -0600

    Merchant show action sends one merchant and their attrivutes by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe "Merchants API" do
 
         get '/api/v1/merchants'
 
-        expect(response.status).to eq(200)
-
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
+        expect(response.status).to eq(200)
         expect(merchants.count).to eq(3)
 
         merchants.each do |merchant|
@@ -27,18 +26,37 @@ RSpec.describe "Merchants API" do
       it "sends an empty array if no merchants exist" do
         get '/api/v1/merchants'
 
-        expect(response.status).to eq(200)
-
         merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
+        expect(response.status).to eq(200)
         expect(merchants).to eq([])
       end
     end
   end
 
-  # describe 'show action' do
-  #   it 'sends data for one merchant' do
+  describe 'GET /api/v1/merchant/:id endpoint' do
+    context 'happy path' do
+      it 'gets one merchant and their attributes by id' do
+        id = create(:merchant).id
 
-  #   end
-  # end
+        get "/api/v1/merchants/#{id}"
+
+        merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+        
+        expect(response.status).to eq(200)
+        expect(merchant).to have_key(:id)
+        expect(merchant[:attributes]).to have_key(:name)
+        expect(merchant[:attributes][:name]).to be_a(String)
+        expect(merchant).to_not have_key(:number_sold)
+      end
+    end
+
+    context 'sad path' do
+      it "sends a 404 status if the id not valid" do
+        get "/api/v1/merchants/1"
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
 end
