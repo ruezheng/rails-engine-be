747c7dd95e7aafc52fc84caa2d8b1c668559e7a3
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-08-29 20:58:40 -0600

    test: expect merchant attributes to have keys name and id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,32 +3,31 @@ require 'rails_helper'
 RSpec.describe "Merchants API" do
   describe 'index action' do
     context 'happy path' do
-      it 'all merchants returned are the same in the db' do
+      it 'gets all merchants and their attributes' do
         create_list(:merchant, 3)
         
         get '/api/v1/merchants'
         
         expect(response.status).to eq(200)      
         
-        merchants = JSON.parse(response.body, symbolize_names: true)
+        merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
         expect(merchants.count).to eq(3)
         
         merchants.each do |merchant|
-          expect(merchant).to have_key(:id) 
-          expect(merchant[:id]).to be_an(Integer) 
-
-          expect(merchant).to have_key(:name) 
-          expect(merchant[:name]).to be_a(String) 
+          expect(merchant[:attributes]).to include(:id, :name) 
+          expect(merchant[:attributes][:id]).to be_an(Integer) 
+          expect(merchant[:attributes][:name]).to be_a(String) 
         end
       end
     end
+    
+    context 'sad path' do
+      expect(response).to_not have_key(:number_sold)
+      expect(error).to eq(null)
+    end
+
   end
-  #   context 'sad path' do
-  #     expect(response).to_not have_key(:number_sold) 
-  #     expect(error).to eq(null)
-  #   end
-  # end  
 
   # describe 'show action' do
   #   it 'sends data for one merchant' do
