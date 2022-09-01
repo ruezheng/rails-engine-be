59663b940e4962bbd1b60ae773a3be47aef815b6
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-09-01 10:30:18 -0600

    Feat: add index action to ItemMerchantController

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::MerchantItemsController < ApplicationController
 
   def index
     if Merchant.exists?(params[:merchant_id])
-      merchant = Merchant.find(params[:merchant_id])
-      render json: ItemSerializer.new(merchant.items)
-    else 
+      items = Merchant.find(params[:merchant_id]).items
+      render json: ItemSerializer.new(items)
+    else
       message = { error: 'Merchant does not exist' }
       render json: message.to_json, status: 404
     end
